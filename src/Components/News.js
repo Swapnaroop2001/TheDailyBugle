d0eea329a81f018fcbdac2fb915783919c42fb76
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-22 16:44:14 -0500

    Fetching

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,26 +2,46 @@ import React, { Component } from 'react'
 import NewsCard from './NewsCrad'
 
 export default class News extends Component {
+    articles = []
+    constructor() {
+        super();
+        this.state = {
+            A: this.articles
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=b79cc641fca24f60b82db250210a261d"
+        let data = await fetch(url);
+        let ParsedData = await data.json();
+        console.log(ParsedData);
+        this.setState({
+            A: ParsedData.articles
+        })
+
+
+    }
+
+
     render() {
         return (
             <div className='container'>
-                <h1 style={{textAlign:"center"}}>Top Headlines</h1>
+                <h1 style={{ textAlign: "center" }}>Top Headlines</h1>
                 <div className='row'>
-                    <div className='col md-3'>
-                        <NewsCard title="Hello" description="Hey its apple!" />
-                    </div>
-
-                    <div className='col md-3'>
-                        <NewsCard title="Hello" description="Hey its apple!" />
-                    </div>
-
-                    <div className='col md-3'>
-                        <NewsCard title="Hello" description="Hey its apple!" />
-                    </div>
-
-                    <div className='col md-3'>
-                        <NewsCard title="Hello" description="Hey its apple!" />
-                    </div>
+                    {this.state.A.map((Element) => {
+                        if (Element.url == "https://removed.com") {
+                            return null;
+                        } else {
+                            return <div className='col md-3' key={Element.url}>
+                                <NewsCard title={Element.title ? Element.title.slice(0, 45) : " "}
+                                    description={Element.description ? Element.description.slice(0, 95) + "..." : " "}
+                                    ImgUrl={Element.urlToImage? Element.urlToImage:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9oUzyyrkuzmPkDFcUqTv3lPKHQwxmLiAug&usqp=CAU"}
+                                    url={Element.url}
+                                    date={Element.publishedAt.slice(5, 10) + "-" + Element.publishedAt.slice(0, 4)}
+                                />
+                            </div>
+                        }
+                    })}
                 </div>
             </div>
         )
--- src/Components/NewsCrad.js
+++ src/Components/NewsCrad.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 
 export default class NewsCard extends Component {
     render() {
-        let { title, description } = this.props;
+        let { title, description, ImgUrl, url, date } = this.props;
         return (
-            <div className="card" style={{width: "18rem"}}>
-                <img src="..." className="card-img-top" alt="..."/>
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem"}}>
+                    <img src={ImgUrl} className="card-img-top"  style={{height:"500"}} alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/news" className="btn btn-primary">Read more</a>
+                        <p className="card-text" style={{fontFamily:'serif'}}>{description}</p>
+                        <p className="card-text" style={{fontSize:"small", fontFamily:"serif"}} >{date}</p>
+                        <a href={url} className="btn btn-primary">Read more...</a>
                     </div>
+                </div>
             </div>
         )
     }

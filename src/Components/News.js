7f07330a1953253131ea9def37b6a5e2e34debf2
Swapnaroop <swapnaroop2001@gmail.com>
2023-12-22 00:01:02 -0500

    minor update

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react'
 import NewsCard from './NewsCrad'
 
 export default class News extends Component {
-  render() {
-    return (
-      <div className='container my-3 mx-6'>
-        <NewsCard />
-        <NewsCard/>
-        <NewsCard/>
-        <NewsCard/>
-        <NewsCard/>
-        <NewsCard/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container'>
+                <h1 style={{textAlign:"center"}}>Top Headlines</h1>
+                <div className='row'>
+                    <div className='col md-3'>
+                        <NewsCard title="Hello" description="Hey its apple!" />
+                    </div>
+
+                    <div className='col md-3'>
+                        <NewsCard title="Hello" description="Hey its apple!" />
+                    </div>
+
+                    <div className='col md-3'>
+                        <NewsCard title="Hello" description="Hey its apple!" />
+                    </div>
+
+                    <div className='col md-3'>
+                        <NewsCard title="Hello" description="Hey its apple!" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
